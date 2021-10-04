acf5a3ebe997a304c2796ff1652957a83d643602
kyuthewise <n0oise0407@gmail.com>
2021-10-04 23:06:45 +0200

    k

--- src/handlers/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  automodHandler: require("./automod-handler"),
-  counterHandler: require("./counter-handler"),
-  greetingHandler: require("./greeting-handler"),
-  inviteHandler: require("./invite-handler"),
-  musicHandler: require("./music-handler"),
-  reactionHandler: require("./reaction-handler"),
-  xpHandler: require("./xp-handler"),
-};
--- src/structures/index.js
+++
@@ -1,9 +0,0 @@
-const BotClient = require("./BotClient");
-const Command = require("./Command");
-const CommandCategory = require("./CommandCategory");
-
-module.exports = {
-  BotClient,
-  Command,
-  CommandCategory,
-};
